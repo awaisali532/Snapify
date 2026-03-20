00f0392a573b750b3119d82638760b0824032195
Awais Ali <awaisali532193@gmail.com>
2026-03-20 16:15:12 +0500

    "Again Url frontend change"

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://snapify-dks6.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://snapify-dks6-np6ec2v4p-awais-alis-projects-e3dd5bb2.vercel.app",
+    ],
     credentials: true,
   }),
 );
